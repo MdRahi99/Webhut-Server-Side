cc41b5a8eafd3910313e0a995095c9b44f7ab2a1
Md Rahi <fhrfhr102030@gmail.com>
2022-10-26 04:07:29 +0600

    Server Initial Setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
+
+app.get('/', (req, res) => {
+    res.send('Web Hut Api Running');
+});
+
+app.get('/courses-categories', (req, res) => {
+    res.send(courses);
+});
+
+app.listen(port, () => {
+    console.log('Web Hut Api Running', port);
+});
