c5377773b7600240edc9d1b50478bee5405fffa4
Md Rahi <fhrfhr102030@gmail.com>
2022-10-26 18:29:52 +0600

    set some more data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const courseDetails = require('./data/courseDetails.json');
 
 app.get('/', (req, res) => {
     res.send('Web Hut Api Running');
@@ -13,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/courses-categories', (req, res) => {
     res.send(courses);
+});
+
+app.get('/course-category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '00'){
+        res.send(courseDetails);
+    }
+    else{
+        const courseCategory = courseDetails.filter(course => course.category_id === id);
+        res.send(courseCategory);
+    }
+});
+
+app.get('/course-details', (req, res) => {
+    res.send(courseDetails);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courseDetails.find(course => course._id === id);
+    res.send(selectedCourse);
+    console.log(req.params.id);
 });
 
 app.listen(port, () => {
